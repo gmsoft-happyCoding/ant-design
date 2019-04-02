b87d4ecbb613a9229b6ee16a06f02392211274a5
angular-moon <xiaoxuss@163.com>
2019-04-02 21:28:49 +0800

    button custom allow pass children

--- components/button/button-custom.tsx
+++ components/button/button-custom.tsx
@@ -1,62 +1,63 @@
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable import/prefer-default-export */
 import * as React from 'react';
 import Button, { NativeButtonProps } from './button';
 import { Omit } from '../_util/type';
 
-export type CustomButtonProps = Omit<NativeButtonProps, 'type' | 'icon' | 'children'>;
+export type CustomButtonProps = Omit<NativeButtonProps, 'type' | 'icon'>;
 export type CustomButton = React.SFC<CustomButtonProps>;
 
 const Save: CustomButton = props => (
   <Button icon="save" type="primary" {...props}>
-    保存
+    {props.children || '保存'}
   </Button>
 );
 
 const Submit: CustomButton = props => (
   <Button icon="check" type="primary" {...props}>
-    提交
+    {props.children || '提交'}
   </Button>
 );
 
 const Cancel: CustomButton = props => (
   <Button icon="rollback" type="default" {...props}>
-    取消
+    {props.children || '取消'}
   </Button>
 );
 
 const Delete: CustomButton = props => (
   <Button icon="close-circle" type="danger" {...props}>
-    删除
+    {props.children || '删除'}
   </Button>
 );
 
 const Upload: CustomButton = props => (
   <Button icon="upload" type="primary" {...props}>
-    上传
+    {props.children || '上传'}
   </Button>
 );
 
 const Download: CustomButton = props => (
   <Button icon="download" type="primary" {...props}>
-    下载
+    {props.children || '下载'}
   </Button>
 );
 
 const Edit: CustomButton = props => (
   <Button icon="edit" type="primary" {...props}>
-    编辑
+    {props.children || '编辑'}
   </Button>
 );
 
 const Search: CustomButton = props => (
   <Button icon="search" type="primary" {...props}>
-    搜索
+    {props.children || '搜索'}
   </Button>
 );
 
 const View: CustomButton = props => (
   <Button icon="schedule" type="primary" {...props}>
-    查看
+    {props.children || '查看'}
   </Button>
 );
 

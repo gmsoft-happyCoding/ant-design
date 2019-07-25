b93fcd21d183c8f3846c462f641e8aa2bc6f56cf
angular-moon <xiaoxuss@163.com>
2019-07-25 15:52:38 +0800

    defaultDuration 修改为 3s

--- components/message/index.tsx
+++ components/message/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Notification from 'rc-notification';
 import Icon from '../icon';
 
-let defaultDuration = 4;
+let defaultDuration = 3;
 let defaultTop: number;
 let messageInstance: any;
 let key = 1;

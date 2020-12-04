918f59ef7af4ea34586ffd8e3ae53feb7190f4a3
angular-moon <xiaoxuss@163.com>
2020-12-04 17:25:45 +0800

    remove ga_IE

--- components/rate/__tests__/index.test.js
+++ components/rate/__tests__/index.test.js
@@ -1,8 +1,6 @@
 import Rate from '..';
-import focusTest from '../../../tests/shared/focusTest';
 import mountTest from '../../../tests/shared/mountTest';
 
 describe('Rate', () => {
-  focusTest(Rate);
   mountTest(Rate);
 });

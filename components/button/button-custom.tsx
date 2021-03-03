6b2e2be3e4a75b0b58cf6c25b72ba87316e6b270
angular-moon <xiaoxuss@163.com>
2021-03-03 17:23:32 +0800

    improve button-custom

--- components/button/button-custom.tsx
+++ components/button/button-custom.tsx
@@ -1,69 +1,57 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable import/prefer-default-export */
 import * as React from 'react';
 import Button, { NativeButtonProps } from './button';
-import { Omit } from '../_util/type';
 
 export type CustomButtonProps = Omit<NativeButtonProps, 'type' | 'icon'>;
-export type CustomButton = React.SFC<CustomButtonProps>;
 
-const Save: CustomButton = props => (
-  <Button icon="save" type="primary" {...props}>
-    {props.children || '保存'}
+const Save = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="save" type="primary" {...restProps}>
+    {children || '保存'}
+  </Button>
+);
+const Submit = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="check" type="primary" {...restProps}>
+    {children || '提交'}
+  </Button>
+);
+const Cancel = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="rollback" type="default" {...restProps}>
+    {children || '取消'}
+  </Button>
+);
+const Delete = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="close-circle" type="danger" {...restProps}>
+    {children || '删除'}
+  </Button>
+);
+const Upload = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="upload" type="primary" {...restProps}>
+    {children || '上传'}
+  </Button>
+);
+const Download = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="download" type="primary" {...restProps}>
+    {children || '下载'}
+  </Button>
+);
+const Edit = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="edit" type="primary" {...restProps}>
+    {children || '编辑'}
+  </Button>
+);
+const Search = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="search" type="primary" {...restProps}>
+    {children || '搜索'}
   </Button>
 );
 
-const Submit: CustomButton = props => (
-  <Button icon="check" type="primary" {...props}>
-    {props.children || '提交'}
+const View = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="schedule" type="primary" {...restProps}>
+    {children || '查看'}
   </Button>
 );
-
-const Cancel: CustomButton = props => (
-  <Button icon="rollback" type="default" {...props}>
-    {props.children || '取消'}
-  </Button>
-);
-
-const Delete: CustomButton = props => (
-  <Button icon="close-circle" type="danger" {...props}>
-    {props.children || '删除'}
-  </Button>
-);
-
-const Upload: CustomButton = props => (
-  <Button icon="upload" type="primary" {...props}>
-    {props.children || '上传'}
-  </Button>
-);
-
-const Download: CustomButton = props => (
-  <Button icon="download" type="primary" {...props}>
-    {props.children || '下载'}
-  </Button>
-);
-
-const Edit: CustomButton = props => (
-  <Button icon="edit" type="primary" {...props}>
-    {props.children || '编辑'}
-  </Button>
-);
-
-const Search: CustomButton = props => (
-  <Button icon="search" type="primary" {...props}>
-    {props.children || '搜索'}
-  </Button>
-);
-
-const View: CustomButton = props => (
-  <Button icon="schedule" type="primary" {...props}>
-    {props.children || '查看'}
-  </Button>
-);
-
-const Add: CustomButton = props => (
-  <Button icon="plus-circle" type="primary" {...props}>
-    {props.children || '新增'}
+const Add = ({ children, ...restProps }: CustomButtonProps) => (
+  <Button icon="plus-circle" type="primary" {...restProps}>
+    {children || '新增'}
   </Button>
 );
 
